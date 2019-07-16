53d4a452645ae280c2192c4a291a7c7cfdbb763b
Marcel <suterma@users.noreply.github.com>
2019-07-17 00:14:21 +0200

    Update WeatherPress.py

--- WeatherPress.py
+++ WeatherPress.py
@@ -18,8 +18,7 @@ s.set_resolution(0)
 temperature = s.temperature()
 humidity = s.humidity()
 
-print("{:.2f} °C".format(temperature))
-print("{:.2f} %rH".format(humidity))
+print("{:.3f} °C".format(temperature) + " | {:.3f} %rH".format(humidity))
 
 s.cancel()
 pi.stop()
@@ -30,20 +29,15 @@ from wordpress_xmlrpc import Client
 from wordpress_xmlrpc.methods import posts
 from wordpress_xmlrpc import WordPressPost
 
-textHumidity = "Relative Humidity is : %.2f%%" %humidity
-textTemperature = "Temperature in Celsius is : %.2f°C" %temperature
-
-print (textHumidity)
-print (textTemperature)
-
-postTitleHumidity = "%.1f%%" %humidity
-postTitleTemperature = "%.1f°C" %temperature
+textHumidity = "Relative Humidity is : %.3f %%rH" %humidity
+textTemperature = "Temperature in Celsius is : %.3f °C" %temperature
 
 print('preparing post...')
 
 import yaml
 with open("WeatherPress.config.yml", "r") as configFile:
     config = yaml.safe_load(configFile)
+
 blog = Client(config['WordPress']['xmlRcpApiUrl'],
               config['WordPress']['username'],
               config['WordPress']['password'])
@@ -51,7 +45,10 @@ blog = Client(config['WordPress']['xmlRcpApiUrl'],
 print('posting...')
 
 post = WordPressPost()
-post.title = postTitleTemperature + " " + postTitleHumidity
+# Create a title with some simple styling classes
+post.title = ("{:.1f} <span class='unity'>°C</span>".format(temperature) +
+              "&nbsp;&nbsp;&nbsp;"+
+              "{:.0f} <span class='unity'>%rH</span>".format(humidity))
 post.slug='MY_POST_PERMANENT_LINK'
 post.content = textTemperature + ' ' + textHumidity
 post.terms_names = {
@@ -60,11 +57,12 @@ post.terms_names = {
 }
 post.id = blog.call(posts.NewPost(post))
 # Always publish these posts
+print('publishing...')
 post.post_status = 'publish'
 blog.call(posts.EditPost(post.id, post))
 
 # Report success
-print(post.title + ' posted to ' + xmlRcpApiUrl)
+print(post.title + ' publicly posted to ' + config['WordPress']['xmlRcpApiUrl'])
 
 
 
