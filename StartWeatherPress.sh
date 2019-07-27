b77b2e7e2a7642e656b7e191f56d48f5fee808f6
Marcel <suterma@users.noreply.github.com>
2019-07-27 23:48:49 +0200

    Update StartWeatherPress.sh

--- StartWeatherPress.sh
+++ StartWeatherPress.sh
@@ -2,7 +2,7 @@
 # Prepares the pigpiod daemon and runs the WeatherPress python script
 
 # start the daemon, if not running
-if [ps -u root | grep pigpiod -q] then
+if ! ps -u root | grep -q pigpiod; then
     echo "starting pigpiod..."
     sudo pigpiod
 fi
